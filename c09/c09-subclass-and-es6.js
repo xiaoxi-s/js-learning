51552aa65e357cde3c6274314e027962d1724929
xiaoxi <77871407@qq.com>
2023-03-12 18:51:51 -0500

    update setPropertyOf inheritance & static properties

--- c09/c09-subclass-and-es6.js
+++ c09/c09-subclass-and-es6.js
@@ -16,3 +16,32 @@ console.log(Array.prototype.isPrototypeOf(EZArray.prototype)); // => true
 console.log(Array.isPrototypeOf(EZArray)); // => true
 console.log(Array.isPrototypeOf(EZArray.prototype)); // => false
 console.log(Array === EZArray.prototype); // => false
+
+
+function Base() {
+}
+Base.baseStaticVar = 'base static var'
+
+Base.prototype.baseInstanceVar = 'base instance var'
+
+function Derived() {
+
+}
+
+// set instance properties
+console.log(Derived.prototype) // {}
+Object.setPrototypeOf(Derived.prototype, Base.prototype)
+console.log(Derived.prototype) // Base {}
+
+// set static properties
+Object.setPrototypeOf(Derived, Base)
+
+// both used together are equivalent as => class Derived extends Base {}
+
+// print instance var
+console.log(new Base().baseInstanceVar)
+console.log(new Derived().baseInstanceVar)
+
+// print static var
+console.log(Base.baseStaticVar)
+console.log(Derived.baseStaticVar)
