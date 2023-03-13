46449b7ae99bb82523777b9f2579c61da78f3d03
xiaoxi <77871407@qq.com>
2023-03-12 19:05:10 -0500

    for of valid parameters

---
+++ c07/c07-for-of-undefined-and-empty-array.js
@@ -0,0 +1,11 @@
+arr1 = undefined
+arr2 = []
+
+// only an iterable obj can be used in for-of loop
+// for (let my_var of arr1) {
+//     console.log(my_var)
+// } 
+
+for (let my_var of arr2) {
+    console.log(my_var)
+} 
